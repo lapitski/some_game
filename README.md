936d51086907d2df27897909ab11a1c979e5071d
andrey <7media73@gmail.com>
2024-02-12 19:59:56 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# some_game
+
+A new Flutter project.
---
+++ lib/levels/level.dart
@@ -0,0 +1,14 @@
+import 'dart:async';
+
+import 'package:flame/components.dart';
+import 'package:flame_tiled/flame_tiled.dart';
+
+class Level extends World {
+  late TiledComponent level;
+  @override
+  FutureOr<void> onLoad() async{
+
+    level = await TiledComponent.load(fileName, destTileSize)
+    return super.onLoad();
+  }
+}
